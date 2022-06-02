c67baa56f347cc07e11672623e781c9d6b8af79d
st084331 <st084331@student.spbu.ru>
2022-06-03 00:01:59 +0300

    ups bad commit

---
+++ WeatherNETwin/WeatherNETwin/APIcallback.cs
@@ -0,0 +1,90 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherNETwin
+{
+    public static class APIcallback
+    {
+        private static string exchangerateAPI = "https://v6.exchangerate-api.com/v6/aaa255ecc18c08444653828f/latest/USD";
+        private static string currencyfreaksAPI = "https://api.currencyfreaks.com/latest?apikey=f751f83125c44cd385171bcfae437c79";
+        private static string currencyapiAPI = "https://api.currencyapi.com/v3/latest?apikey=FVjRXEjsOd5uJPwWfo6a6EXvmDJGdhu19YFynWoM";
+        private static string openexchangeratesAPI = "https://openexchangerates.org/api/latest.json?app_id=ba9500c975284e3a90cfe80e3cf27a21&base=USD";
+
+        public static string getInfoByAPI(string url)
+        {
+            string CorrectUrl = String.Format(url);
+            WebRequest requestObj = WebRequest.Create(CorrectUrl);
+            requestObj.Method = "GET";
+            HttpWebResponse responseObj = null;
+            responseObj = (HttpWebResponse)requestObj.GetResponse();
+            string result = null;
+            using (Stream stream = responseObj.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(stream);
+                result = reader.ReadToEnd();
+                reader.Close();
+            }
+            return result;
+        }
+         
+        public static string[] currencyapiDataForRegression()
+        {
+            string[] ratesPerDay = new string[6];
+            for(int i = 0; i < 6; i++)
+            {
+                string apiURL = "https://api.currencyapi.com/v3/historical?apikey=FVjRXEjsOd5uJPwWfo6a6EXvmDJGdhu19YFynWoM&currencies=RUB&date=" + DateTime.Now.AddDays(-(i+1)).ToString("yyyy/MM/dd").Replace('.', '-');
+                string jsonFormat = getInfoByAPI(apiURL);
+                JObject obj = JObject.Parse(jsonFormat);
+                ratesPerDay[i] = obj["data"]["RUB"]["value"].ToString().Replace('.', ',');
+            }
+            return ratesPerDay;
+        }
+
+        public static string[] openexchangeratesDataForRegression()
+        {
+            string[] ratesPerDay = new string[6];
+            for (int i = 0; i < 6; i++)
+            {
+                string apiURL = "https://openexchangerates.org/api/historical/"+ DateTime.Now.AddDays(-(i + 1)).ToString("yyyy/MM/dd").Replace('.', '-') + ".json?app_id=ba9500c975284e3a90cfe80e3cf27a21";
+                string jsonFormat = getInfoByAPI(apiURL);
+                JObject obj = JObject.Parse(jsonFormat);
+                ratesPerDay[i] = obj["rates"]["RUB"].ToString().Replace('.', ','); ;
+            }
+            return ratesPerDay;
+        }
+
+        public static string getExchangerate()
+        {
+            string jsonFormat = getInfoByAPI(exchangerateAPI);
+            JObject obj = JObject.Parse(jsonFormat);
+            return obj["conversion_rates"]["RUB"].ToString().Replace('.', ','); ;
+        }
+
+        public static string getOpenexchangerates()
+        {
+            string jsonFormat = getInfoByAPI(openexchangeratesAPI);
+            JObject obj = JObject.Parse(jsonFormat);
+            return obj["rates"]["RUB"].ToString().Replace('.', ','); ;
+        }
+
+        public static string getCurrencyapi()
+        {
+            string jsonFormat = getInfoByAPI(currencyapiAPI);
+            JObject obj = JObject.Parse(jsonFormat);
+            return obj["data"]["RUB"]["value"].ToString().Replace('.', ','); ;
+        }
+
+        public static string getCurrencyfreaks()
+        {
+            string jsonFormat = getInfoByAPI(currencyfreaksAPI);
+            JObject obj = JObject.Parse(jsonFormat);
+            return obj["rates"]["RUB"].ToString().Replace('.', ','); ;
+        }
+    }
+}
--- WeatherNETwin/WeatherNETwin/MainWindow.xaml.cs
+++ WeatherNETwin/WeatherNETwin/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Accord.Statistics.Models.Regression.Linear;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,53 @@ namespace WeatherNETwin
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string todayCurrencyapi = APIcallback.getCurrencyapi();
+            string todayOpenexchangerates = APIcallback.getOpenexchangerates();;
+            Exchangerate.Content = "Exchangerate: " + APIcallback.getExchangerate();
+            Currencyfreaks.Content = "Currencyfreaks: " + APIcallback.getCurrencyfreaks();
+            Currencyapi.Content = "Currencyapi: " + todayCurrencyapi;
+            Openexchangerates.Content = "Openexchangerates: " + todayOpenexchangerates;
+            Load_Plot(todayCurrencyapi, todayOpenexchangerates);
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            Exchangerate.Content = "Exchangerate: " + APIcallback.getExchangerate();
+            Currencyfreaks.Content = "Currencyfreaks: " + APIcallback.getCurrencyfreaks();
+            Currencyapi.Content = "Currencyapi: " + APIcallback.getCurrencyapi();
+            Openexchangerates.Content = "Openexchangerates: " + APIcallback.getOpenexchangerates();
+        }
+
+        private void Load_Plot(string todayCurrencyapi, string todayOpenexchangerates)
+        {
+            string[] regres1 = APIcallback.currencyapiDataForRegression();
+            string[] regres2 = APIcallback.openexchangeratesDataForRegression();
+            double[] y = new double[14];
+            double[] x = new double[14];
+            y[0] = Convert.ToDouble(todayCurrencyapi);
+            y[7] = Convert.ToDouble(todayOpenexchangerates);
+            x[0] = 1;
+            x[7] = 1;
+            for (int i = 0; i < 6; i++)
+            {
+                x[i + 1] = i + 2;
+                x[i + 8] = i + 2;
+                y[i + 1] = Convert.ToDouble(regres1[i]);
+                y[i + 8] = Convert.ToDouble(regres2[i]);
+            }
+            var ls = new PolynomialLeastSquares()
+            {
+                Degree = 2
+            };
+            PolynomialRegression poly = ls.Learn(x, y);
+            double[] res = new double[] { 1, 2, 3, 4, 5, 6, 7 };
+            double[] pred = poly.Transform(res);
+            RegressionPlot.Plot.AddScatter(res, pred);
+            RegressionPlot.Refresh();
         }
     }
 }
